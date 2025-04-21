f26002914ae80074bd664abac23e9179a7e15508
Maxim Focusov <137664115+nez1d@users.noreply.github.com>
2025-04-21 15:55:33 +0300

    Delete ShopDevelop.Microservice.Cache directory

--- ShopDevelop.Microservice.Cache/Services/ICacheService.cs
+++
@@ -1,7 +0,0 @@
-namespace ShopDevelop.Microservice.Cache.Services;
-
-public interface ICacheService
-{
-    Task SetCacheAsync(string key, string? data, uint seconds);
-    Task<string> GetCacheAsync(string key, HttpContext context);
-}
