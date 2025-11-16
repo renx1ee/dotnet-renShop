8c8d289a17d202bf64c72b0037b45891de8498b3
renx1ee <maksimgolavnoy@gmail.com>
2025-11-17 02:38:59 +0300

    Add unit of work pattern

---
+++ src/RenStore.Application/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+using RenStore.Domain.Repository;
+
+namespace RenStore.Application.UnitOfWork;
+
+public interface IUnitOfWork : IDisposable
+{
+    IProductRepository Products { get; }
+    IProductClothRepository ProductClothes { get; }
+    IProductClothSizeRepository ProductClothSizes { get; }
+    IProductVariantRepository ProductVariants { get; }
+    IProductDetailRepository ProductDetails { get; }
+    IProductPriceHistoryRepository PriceHistories{ get; }
+    IProductAttributeRepository ProductAttributes { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken calcellationToken);
+    Task BeginTransactionAsync(CancellationToken calcellationToken);
+    Task CommitAsync(CancellationToken calcellationToken);
+    Task RollbackAsync(CancellationToken calcellationToken);
+}
---
+++ src/RenStore.Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,58 @@
+using Microsoft.EntityFrameworkCore.Storage;
+using RenStore.Application.UnitOfWork;
+using RenStore.Domain.Repository;
+
+namespace RenStore.Persistence.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    
+    public IProductRepository Products { get; private set; }
+    public IProductClothRepository ProductClothes { get; private set; }
+    public IProductClothSizeRepository ProductClothSizes { get; private set; }
+    public IProductVariantRepository ProductVariants { get; private set; }
+    public IProductDetailRepository ProductDetails { get; private set; }
+    public IProductPriceHistoryRepository PriceHistories { get; private set; }
+    public IProductAttributeRepository ProductAttributes { get; private set; }
+
+    private IDbContextTransaction _transaction;
+
+    public UnitOfWork(
+        ApplicationDbContext context,
+        IProductRepository productRepository,
+        IProductClothRepository productClothRepository,
+        IProductClothSizeRepository productClothSizeRepository,
+        IProductVariantRepository productVariantRepository,
+        IProductDetailRepository productDetailsRepository,
+        IProductPriceHistoryRepository priceHistoryRepository,
+        IProductAttributeRepository productAttributeRepository)
+    {
+        this._context = context;
+        this.Products = productRepository;
+        this.ProductClothes = productClothRepository;
+        this.ProductClothSizes = productClothSizeRepository;
+        this.ProductVariants = productVariantRepository;
+        this.ProductDetails = productDetailsRepository;
+        this.PriceHistories = priceHistoryRepository;
+        this.ProductAttributes = productAttributeRepository;
+    }
+    
+    public async Task<int> SaveChangesAsync(CancellationToken calcellationToken) =>
+        await _context.SaveChangesAsync(calcellationToken);
+    
+    public async Task BeginTransactionAsync(CancellationToken calcellationToken) =>
+        _transaction = await _context.Database.BeginTransactionAsync(calcellationToken);
+    
+    public async Task CommitAsync(CancellationToken calcellationToken) =>
+        await _transaction.CommitAsync(calcellationToken);
+    
+    public async Task RollbackAsync(CancellationToken calcellationToken) =>
+        await _transaction.RollbackAsync(calcellationToken);
+    
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+        _context.Dispose();
+    }
+}
